e66c25ca4c64e18cda2c5bbca56d11c14c60b6d1
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2025-07-16 22:53:27 +0300

    Forgot to add test file in prev commit

---
+++ test/opaque_test.erl
@@ -0,0 +1,52 @@
+-module(opaque_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include("../include/erldantic.hrl").
+-include("../include/erldantic_internal.hrl").
+
+-compile(nowarn_unused_type).
+
+-opaque person() :: #{name => binary(), age => pos_integer()}.
+
+-record(my_rec, {id :: integer(), data}).
+
+-opaque my_rec_t() :: #my_rec{data :: person()}.
+
+-export_type([person/0, my_rec_t/0]).
+
+simple_test() ->
+    {ok, Types} = erldantic_abstract_code:types_in_module(?MODULE),
+
+    %% Normal opaque type
+    ?assertEqual(#a_map{fields =
+                            [{map_field_assoc, name, {type, binary}},
+                             {map_field_assoc, age, {type, pos_integer}}]},
+                 maps:get({type, person, 0}, Types)),
+    Person = #{name => <<"John">>, age => 42},
+    ?assertEqual({ok, Person}, erldantic_json:type_to_json(?MODULE, person, Person)),
+    ?assertEqual({ok, Person},
+                 erldantic_json:type_from_json(?MODULE,
+                                               person,
+                                               #{<<"name">> => <<"John">>, <<"age">> => 42})),
+
+    %% Opaque record type
+    ?assertEqual(#a_rec{name = my_rec,
+                        fields = [{id, {type, integer}}, {data, {type, term}}],
+                        arity = 3},
+                 maps:get({record, my_rec}, Types)),
+    ?assertEqual({record_ref, my_rec, [{data, {user_type_ref, person, []}}]},
+                 maps:get({type, my_rec_t, 0}, Types)),
+
+    ?assertEqual({ok, #{id => 1, data => #{name => <<"John">>, age => 42}}},
+                 erldantic_json:type_to_json(?MODULE,
+                                             my_rec_t,
+                                             #my_rec{id = 1,
+                                                     data = #{name => <<"John">>, age => 42}})),
+    ?assertEqual({ok, #my_rec{id = 1, data = #{name => <<"John">>, age => 42}}},
+                 erldantic_json:type_from_json(?MODULE,
+                                               my_rec_t,
+                                               #{<<"id">> => 1,
+                                                 <<"data">> =>
+                                                     #{<<"name">> => <<"John">>,
+                                                       <<"age">> => 42}})).
