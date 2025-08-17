68b252136840aa693718e2dbdc0e8dd07f628005
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2025-08-17 11:48:18 +0200

    ignore xref for exported for tests

--- src/erldantic_json.erl
+++ src/erldantic_json.erl
@@ -1,8 +1,10 @@
 -module(erldantic_json).
 
 -export([type_to_json/3, type_from_json/3, record_to_json/3, record_from_json/3]).
+
 %% exported for test
 -export([to_json/3, from_json/3]).
+-ignore_xref([to_json/3, from_json/3]).
 
 -ignore_xref([{erldantic_json, type_to_json, 3},
               {erldantic_json, type_from_json, 3},
